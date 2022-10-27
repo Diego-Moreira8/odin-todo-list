5fcf86edff7bfd051b3f1650d92e51f09f92350e
Diego Moreira <diego.moreira529@gmail.com>
2022-10-27 10:18:07 -0300

    Created allProjects obj and addProject()

--- index.js
+++ index.js
@@ -17,33 +17,99 @@ class Project {
   }
 }
 
-function renderMenu() {
-  let menuDiv = document.createElement("div");
+// Tests ------------------------------------------------------------------
+const allProjects = {
+  projectsArray: [
+    new Project("default"),
+    new Project("Teste 1"),
+    new Project("Teste 2"),
+  ],
 
-  for (let project of projects) {
+  addProject() {
+    allProjects.projectsArray.push(new Project("Novo Projeto"));
+    renderMenu();
+  },
+
+  removeProject() {
+    alert("Remove Project");
+  },
+};
+
+allProjects.projectsArray.push(new Project("Teste 3"));
+
+allProjects.projectsArray[0].addToDoItem(
+  "Fazer compras",
+  "Descrição",
+  "28/10/22",
+  false
+);
+allProjects.projectsArray[0].addToDoItem(
+  "Limpar mesa",
+  "Descrição",
+  "26/10/22",
+  true
+);
+allProjects.projectsArray[1].addToDoItem(
+  "Lavar louças",
+  "Descrição",
+  "28/10/22",
+  false
+);
+allProjects.projectsArray[1].addToDoItem(
+  "Tomar banho",
+  "Descrição",
+  "28/10/22",
+  false
+);
+allProjects.projectsArray[1].addToDoItem(
+  "Estudar",
+  "Descrição",
+  "28/10/22",
+  false
+);
+allProjects.projectsArray[3].addToDoItem(
+  "Pescar",
+  "Descrição",
+  "28/10/22",
+  false
+);
+// Tests ------------------------------------------------------------------
+
+function renderMenu() {
+  const menuDiv = document.querySelector("#menu");
+  menuDiv.innerHTML = "";
+
+  for (let project of allProjects.projectsArray) {
     menuDiv.insertAdjacentHTML(
       "beforeend",
-      `<button class="project" data-project-index=${projects.indexOf(
+      `<button class="project" data-project-index=${allProjects.projectsArray.indexOf(
         project
       )}>${project.name}</button>`
     );
   }
 
-  // Adding event listeners
+  // Adding event listeners to the projects buttons
   const menuButtons = menuDiv.querySelectorAll(".project");
   menuButtons.forEach((button) => {
     button.addEventListener("click", (e) => {
       const index = e.target.getAttribute("data-project-index");
-      renderToDosArea(projects[index]);
+      renderToDosArea(allProjects.projectsArray[index]);
     });
   });
+
+  // Create ADD PROJECT button
+  const addProjectBtn = document.createElement("button");
+  addProjectBtn.textContent = "+";
+  addProjectBtn.id = "add-project";
+  addProjectBtn.addEventListener("click", allProjects.addProject);
+  menuDiv.appendChild(addProjectBtn);
 
   return menuDiv;
 }
 
 function renderToDosArea(project) {
-  let toDosAreaDiv = document.createElement("div");
-  toDosAreaDiv.classList.add("todos-area-div");
+  const toDosAreaDiv = document.querySelector("#todos-area-div");
+  toDosAreaDiv.innerHTML = "";
 
   for (let item of project.toDos) {
     toDosAreaDiv.insertAdjacentHTML(
@@ -55,18 +121,9 @@ function renderToDosArea(project) {
   return toDosAreaDiv;
 }
 
-const projects = [
-  new Project("default"),
-  new Project("Teste 1"),
-  new Project("Teste 2"),
-];
-projects[0].addToDoItem("Fazer compras", "Descrição", "28/10/22", false);
-projects[0].addToDoItem("Limpar mesa", "Descrição", "26/10/22", true);
-projects[1].addToDoItem("Lavar louças", "Descrição", "28/10/22", false);
-projects[1].addToDoItem("Tomar banho", "Descrição", "28/10/22", false);
-projects[1].addToDoItem("Estudar", "Descrição", "28/10/22", false);
-
+// Initialize Menu
 const menuDiv = document.querySelector("#menu");
 menuDiv.appendChild(renderMenu());
 
-const toDosAreaDiv = document.querySelector("#todos-area-div");
+// Initialize on Default
+renderToDosArea(allProjects.projectsArray[0]);
